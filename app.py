12b38bebd83eb3d5ac82bf28b7bb2d9e101b85d1
Shivam <shivamsaxenagr8@gmail.com>
2020-10-28 01:18:42 +0530

    commitTwo

--- app.py
+++ app.py
@@ -6,62 +6,92 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
+	videoHeight = 500;
 	video_source = "https://gcloud.live/v/rywg2hed--yjmd2?jwsource=cl";
-	return render_template("index.html", video_source = video_source )
+	return render_template("index.html", video_source = video_source, videoHeight = videoHeight)
 
 @app.route('/about')
 
 def about():
-	return render_template("about.html", video_source = video_source)
+	return render_template("about.html")
 
 @app.route('/collection')
 
 def collection():
+	videoHeight = 500;
 	title = "Mirzapur season 2";
-	return render_template("collection.html", title=title, video_source = video_source)
+	video_source = "https://gcloud.live/v/rywg2hed--yjmd2?jwsource=cl";
+	return render_template("collection.html", title=title, video_source = video_source, videoHeight = videoHeight)
 
 @app.route('/episode2')
 def episode2():
+	title = "Mirzapur season 2";
 	sno = "TWO";
-	return render_template("episode2.html", sno=sno, video_source = video_source)
+	videoHeight = 500;
+	video_source = "https://gcloud.live/v/ky5g0h3w44m35nn?jwsource=cl";
+	return render_template("episode2.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 
 @app.route('/episode3')
 def episode3():
 	sno = "THREE";
-	return render_template("episode3.html", sno=sno, video_source = video_source)
+	videoHeight = 500;
+	title = "Mirzapur season 2";
+	video_source = "https://gcloud.live/v/8p40ni8mllp248-?jwsource=cl";
+	return render_template("episode3.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 @app.route('/episode4')
 def episode4():
 	sno = "FOUR";
-	return render_template("episode4.html", sno=sno, video_source = video_source)
+	videoHeight = 500;
+	title = "Mirzapur season 2";
+	video_source = "https://gcloud.live/v/pyjgzhmk11785y3?jwsource=cl";
+	return render_template("episode4.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 @app.route('/episode5')
 def episode5():
 	sno = "FIVE";
-	return render_template("episode5.html", sno=sno, video_source = video_source)
+	videoHeight = 500;
+	title = "Mirzapur season 2";
+	video_source = "https://gcloud.live/v/w3ygxbnmjjnzm4w?jwsource=cl";
+	return render_template("episode5.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 @app.route('/episode6')
 def episode6():
 	sno = "SIX";
-	return render_template("episode6.html", sno=sno, video_source = video_source)
+	videoHeight = 500;
+	title = "Mirzapur season 2";
+	video_source = "https://gcloud.live/v/36d3ecmreemrmqg?jwsource=cl";
+	return render_template("episode6.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 @app.route('/episode7')
 def episode7():
 	sno = "SEVEN";
-	return render_template("episode7.html", sno=sno, video_source = video_source)
+	videoHeight = 500;
+	title = "Mirzapur season 2";
+	video_source = "https://gcloud.live/v/ky5g0h3w448eg07?jwsource=cl";
+	return render_template("episode7.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 @app.route('/episode8')
 def episode8():
 	sno = "EIGHT";
-	return render_template("episode8.html", sno=sno, video_source = video_source)
+	videoHeight = 500;
+	title = "Mirzapur season 2";
+	video_source = "https://gcloud.live/v/ky5g0h3w44371nd?jwsource=cl";
+	return render_template("episode8.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 @app.route('/episode9')
 def episode9():
 	sno = "NINE";
-	return render_template("episode9.html", sno=sno, video_source = video_source)
+	title = "Mirzapur season 2";
+	videoHeight = 500;
+	video_source = "https://gcloud.live/v/qy5gmhen006g11p?jwsource=cl";
+	return render_template("episode9.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
 
 @app.route('/episode10')
 def episode10():
 	sno = "TEN";
-	return render_template("episode10.html", sno=sno, video_source = video_source)
+	title = "Mirzapur season 2";
+	videoHeight = 500;
+	video_source = "https://gcloud.live/v/7r3dgbgkqqg63jw?jwsource=cl";
+	return render_template("episode10.html", sno=sno, video_source = video_source, title=title, videoHeight = videoHeight)
